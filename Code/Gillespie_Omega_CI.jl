e24382d7ada25d7f6d23152b6dada9af9dfcc6db
Devon Kohler <devonjkohler@gmail.com>
2020-11-23 17:10:37 -0500

    Added code

--- Code/Gillespie_Omega_CI.jl
+++ Code/Gillespie_Omega_CI.jl
@@ -2,8 +2,8 @@
 ## Load Packages
 using Omega
 using StatsBase
-using Plots
 using Random
+using Plots
 using Distributions
 
 Random.seed!(123)
@@ -78,12 +78,6 @@ function get_hazards(ecology, theta)
         )
 end
 
-# function sample_hazards(rng, weights)
-#     sample(["test1", "test2", "test3"], Weights(weights))
-# end
-# test = ciid(sample_hazards, [20,50,25])
-# rand(test,5)
-
 # Define gillespie parameters
 Pre = [[1, 0], [1, 1], [0, 1]]
 Post = [[2, 0], [0, 2], [0, 0]]
@@ -110,31 +104,50 @@ gillespie = ciid(gillespie_, transitions, 10000, 0.0)
 samples = rand((prey_init, pred_init, spawn_prey, prey2pred, pred_dies, gillespie),
                 5, alg = RejectionSample)
 
-
-plot_vals = hcat(values(samples[3][6]["prey"]), values(samples[3][6]["pred"]))
+plot_vals = hcat(values(samples[2][6]["prey"]), values(samples[2][6]["pred"]))
 plot(1:10001, plot_vals[1:10001,1:2],
         title = "Basic Simulation",
         label = ["Prey" "Pred"],
         lw = 1.25)
 
 ## Sample with intervention
-++
+samples_int = rand((prey_init, pred_init, spawn_prey, prey2pred, pred_dies, gillespie),
+                prey_init > 120, 5, alg = RejectionSample)
 
 plot_vals = hcat(values(samples[2][6]["prey"]), values(samples_int[2][6]["prey"]))
-plot(1:5001, plot_vals[1:5001,1:2],
+plot(1:10001, plot_vals[1:10001,1:2],
         title = "Intervention Comparison",
         label = ["Prey No Intervention" "Prey > 120"],
         lw = 1.25)
 
 plot_vals = hcat(values(samples_int[2][6]["prey"]), values(samples_int[2][6]["pred"]))
-plot(1:5001, plot_vals[1:5001,1:2],
+plot(1:10001, plot_vals[1:10001,1:2],
         title = "Intervention Simulation",
         label = ["Prey" "Pred"],
         lw = 1.25)
 
-x = samples[2][6][1000]["prey"] - samples_int[2][6][1000]["prey"]
-histogram(x)
+# Histogram
+function intervention_hist(sim1, sim2, b)
 
+    sim1_final_vals = zeros(0)
+    sim2_final_vals = zeros(0)
+    for i in 1:length(sim1)
+        append!(sim1_final_vals, sim1[1][6]["prey"][i])
+        append!(sim2_final_vals, sim2[1][6]["prey"][i])
+    end
+
+    final_val_diff = sim1_final_vals - sim2_final_vals
+    histogram(final_val_diff, bins = b)
+end
+
+samples_100 = rand((prey_init, pred_init, spawn_prey, prey2pred, pred_dies, gillespie),
+                100, alg = RejectionSample)
+samples_int_100 = rand((prey_init, pred_init, spawn_prey, prey2pred, pred_dies, gillespie),
+                prey_init > 120, 100, alg = RejectionSample)
+
+intervention_hist(samples,samples_int, 20)
+
+# Sample Rate
 samples_rate_int = rand((prey_init, pred_init, spawn_prey, prey2pred, pred_dies, gillespie),
                     spawn_prey > 1.2, 5, alg = RejectionSample)
 
@@ -145,75 +158,78 @@ plot(1:5001, plot_vals[1:5001,1:2],
         lw = 1.25)
 
 ## Idea for intervention at t_now
-#   Create a new gillespie function (gillespie_new) that
-#   has a paramter for changing a value at t_now. Use replace
-#   to update simulation from old gillespie to gillespie_new.
-#   This should give all the same values until t_now, which
-#   then will update to whatever intervention was set.
-#   I think this is how they do it in the Omega paper.
-function gillespie_time_intervention_(rng, transitions, N, time, update,t0=0.0)
-
-    """
-    Implementation of the Gillespie algorithm for Lotka-Volterra model
-
-    Args:
-    transitions(dict): A dictionary with keys "prey" and "pred", and each element
-        is a list containing the row of the transition matrix.
-    N(int): Number of desired simulation iterations.
-    t0(float): initial time point.
-    """
-
-    initial = Dict("prey" => prey_init(rng), "pred" => pred_init(rng))
-    trajectory = Dict("prey" => [initial["prey"]], "pred" => [initial["pred"]])
-    times = [t0]
-    ecology = initial
-
-    theta = Dict("spawn_prey" => spawn_prey(rng),
-                 "prey2pred" => prey2pred(rng),
-                 "pred_dies" => pred_dies(rng))
-    t = times[1]
-
-    for i = 1:N
-        hazards = get_hazards(ecology, theta)
-        # Can we replace this with a random variable?
-        # It already samples one of three hazards according to some weights
-        transition = transitions[sample(collect(keys(hazards)), Weights(collect(values(hazards))))]
-        t = t + sum(values(hazards))
-
-        ecology["prey"] += transition[1]
-        ecology["pred"] += transition[2]
-
-        # Enforce only positive integers
-        ecology["prey"] = max(1, ecology["prey"])
-        ecology["pred"] = max(1, ecology["pred"])
-
-        append!(trajectory["prey"], ecology["prey"])
-        append!(trajectory["pred"], ecology["pred"])
-        append!(times, t)
-    end
-
-    return Dict(
-        "prey" => trajectory["prey"],
-        "pred" => trajectory["pred"],
-        "times" => times
-        )
+function sample_weights(rng, hazards)
+    result = sample(collect(keys(hazards)), Weights(collect(values(hazards))))
+    return result
 end
 
+function initial_simulation(rng, ecology, theta, t, transitions)
+    hazards = get_hazards(ecology, theta)
 
-model = (prey_init, pred_init, spawn_prey, prey2pred, pred_dies, gillespie)
+    # Does this make sense?
+    hazard_sample = ciid(sample_weights, hazards)
+    transition = transitions[hazard_sample(rng)]
+    t_new = t + sum(values(hazards))
 
-samples = rand(model,
-                5, alg = RejectionSample)
+    ecology_new_prey = ecology["prey"] + transition[1]
+    ecology_new_pred = ecology["pred"] + transition[2]
+    # Enforce only positive integers
+    ecology_new_prey = max(1, ecology_new_prey)
+    ecology_new_pred = max(1, ecology_new_pred)
 
-gillespie_int = ciid(gillespie_time_intervention_, transitions, 10000, 2000, 100, 0.0)
-gillespie_new = replace(gillespie, gillespie => gillespie_int)
+    return ecology_new_prey, ecology_new_pred, t_new
+end
 
-counter_samples = rand(gillespie_new, 5, alg = RejectionSample)
-counter_samples = rand((prey_init, pred_init, spawn_prey, prey2pred, pred_dies, gillespie_new),
-                     5, alg = RejectionSample)
+function one_simulation(rng, ecology, theta, transitions)
 
-plot_vals = hcat(values(samples[1][6]["prey"]), values(counter_samples[1][6]["prey"]))
-plot(1:10001, plot_vals[1:10001,1:2],
-        title = "Counterfactual Comparison",
-        label = ["Prey No Intervention" "Prey Counterfactual"],
-        lw = 1.25)
+    t = ecology["t"]
+    delete!(ecology, "t")
+    hazards = get_hazards(ecology, theta)
+
+    # Does this make sense?
+    hazard_sample = ciid(sample_weights, hazards)
+    transition = transitions[hazard_sample(rng)]
+    t_new = t + sum(values(hazards))
+
+    ecology_new_prey = ecology["prey"] + transition[1]
+    ecology_new_pred = ecology["pred"] + transition[2]
+    # Enforce only positive integers
+    ecology_new_prey = max(1, ecology_new_prey)
+    ecology_new_pred = max(1, ecology_new_pred)
+
+    return Dict("prey" => ecology_new_prey,
+                "pred" => ecology_new_pred,
+                "t" => t_new)
+end
+
+initial = Dict("prey" => rand(prey_init,1)[1], "pred" => rand(pred_init, 1)[1])
+theta_intial = Dict("spawn_prey" => rand(spawn_prey,1)[1],
+             "prey2pred" => rand(prey2pred,1)[1],
+             "pred_dies" => rand(pred_dies,1)[1])
+
+test2 = ciid(initial_simulation, initial, theta_intial, t0, transitions)
+rand(test2,100)
+
+functions_list = Dict("F0" => ciid(initial_simulation, initial, theta_intial, t0, transitions))
+N = 100
+
+for f in 1:N
+    last = f - 1
+    functions_list["F$f"] = ciid(one_simulation, functions_list["F$last"], theta, transitions)
+end
+
+##
+# maybe need initial run and then consecutive as different functions
+# run1(initial) => run2(run1_output) => run3(run2_output) => run4(run3_output)
+#
+
+F1 = one_simulation
+
+function test_func(x,y)
+    return x + y
+end
+
+a = Symbol("test_func")
+@eval $a(1,2)
+
+Symbol("y") = 20
