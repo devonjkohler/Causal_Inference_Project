23530597df33e92bc6b8db14dd84c7dcf4d44e98
Devon Kohler <devonjkohler@gmail.com>
2020-11-29 15:28:14 -0500

    Updated tnow

--- Code/tnow_intervention_implement_fix.jl
+++ Code/tnow_intervention_implement_fix.jl
@@ -20,8 +20,8 @@ function one_simulation_prey(rng, n, transitions)
     new_prey = prey_val + transition[1]
 
     # Enforce only positive integers
-    new_prey = max(1, new_prey)
-    return new_prey
+    max(1, new_prey)
+
 end
 
 function one_simulation_pred(rng, n, transitions)
@@ -37,11 +37,11 @@ function one_simulation_pred(rng, n, transitions)
     new_pred = pred_val + transition[2]
 
     # Enforce only positive integers
-    new_pred = max(1, new_pred)
-    return new_pred
+    max(1, new_pred)
+
 end
 
-function get_hazards(rng, n, theta)
+function get_hazards(rng, n)
     """
     Compute the hazard function given the current states.  Note that in this
         model the function depends only on state, not time, though this is not
@@ -59,14 +59,20 @@ function get_hazards(rng, n, theta)
     ecology = Dict("prey" => prey_list[n](rng), "pred" => pred_list[n](rng))
 
     hazards = Dict(
-        "spawn_prey" => theta["spawn_prey"][1] * ecology["prey"],
-        "prey2pred" => theta["prey2pred"][1] * ecology["prey"] * ecology["pred"],
-        "pred_dies" => theta["pred_dies"][1] * ecology["pred"]
+        "spawn_prey" => theta(rng)["spawn_prey"] * ecology["prey"],
+        "prey2pred" => theta(rng)["prey2pred"] * ecology["prey"] * ecology["pred"],
+        "pred_dies" => theta(rng)["pred_dies"] * ecology["pred"]
         )
-    result = sample(collect(keys(hazards)), Weights(collect(values(hazards))))
-    return result
+    sample(collect(keys(hazards)), Weights(collect(values(hazards))))
 end
 
+function generate_rates(rng)
+
+    Dict("spawn_prey" => spawn_prey(rng),
+                 "prey2pred" => prey2pred(rng),
+                 "pred_dies" => pred_dies(rng))
+
+end
 
 ## Run simulation
 # Define gillespie parameters
@@ -77,37 +83,30 @@ transitions = Dict("spawn_prey" => transition_mat[1,],
                     "prey2pred" => transition_mat[2,],
                     "pred_dies" => transition_mat[3,])
 #t0=0.0
-N = 10000
+N = 10
 
 # Random variables for starting values
 prey_init = normal(100, 10)
 pred_init = normal(50, 10)
 
 # Random variables for rates
-spawn_prey = normal(1, .1)
+spawn_prey = normal(1., .1)
 prey2pred = normal(.01, .001)
 pred_dies = normal(.5, .05)
-
-theta = Dict("spawn_prey" => rand(spawn_prey,1),
-             "prey2pred" => rand(prey2pred,1),
-             "pred_dies" => rand(pred_dies,1))
-
-# Function to join lists together
-@inline tuplejoin(x) = x
-@inline tuplejoin(x, y) = (x..., y...)
-@inline tuplejoin(x, y, z...) = tuplejoin(tuplejoin(x, y), z...)
 
 ## Sample without intervention
 # Random variables for each step of simulation
 hazards_list = Any[]
 prey_list = Any[]
 pred_list = Any[]
+theta = ciid(generate_rates)
 push!(prey_list, prey_init)
 push!(pred_list, pred_init)
+N = 10000
 
 for f in 2:N
     last = f - 1
-    hazards_temp = ciid(get_hazards, last, theta) # individual step
+    hazards_temp = ciid(get_hazards, last) # individual step
     prey_temp = ciid(one_simulation_prey, last, transitions) # individual step
     pred_temp = ciid(one_simulation_pred, last, transitions) # individual step
     push!(hazards_list, hazards_temp)
@@ -115,11 +114,13 @@ for f in 2:N
     push!(pred_list, pred_temp)
 end
 
-samples = rand(tuplejoin(Tuple(x for x in hazards_list),
-                Tuple(x for x in prey_list),
-                Tuple(x for x in pred_list)),
-                5, alg = RejectionSample)
+random_var_tuple = (Tuple(Any[spawn_prey,prey2pred,pred_dies,theta])...,
+                Tuple(x for x in hazards_list)...,
+                Tuple(x for x in prey_list)...,
+                Tuple(x for x in pred_list)...)
 
+samples = rand(random_var_tuple,
+                2, alg = RejectionSample)
 # extract run results and plot
 prey_vals = []
 pred_vals = []
@@ -137,30 +138,38 @@ plot(hcat(prey_vals,pred_vals),
 hazards_list = Any[]
 prey_list = Any[]
 pred_list = Any[]
+theta = ciid(generate_rates)
 push!(prey_list, prey_init)
 push!(pred_list, pred_init)
+N = 1000
 
 for f in 2:N
     last = f - 1
-    hazards_temp = ciid(get_hazards, last, theta) # individual step
+    hazards_temp = ciid(get_hazards, last) # individual step
     prey_temp = ciid(one_simulation_prey, last, transitions) # individual step
     pred_temp = ciid(one_simulation_pred, last, transitions) # individual step
     push!(hazards_list, hazards_temp)
     push!(prey_list, prey_temp)
     push!(pred_list, pred_temp)
 end
-
-random_var_tuple = tuplejoin(Tuple(x for x in hazards_list),
-                Tuple(x for x in prey_list),
-                Tuple(x for x in pred_list))
-samples = rand(random_var_tuple, random_var_tuple[15000] > 400.0,
+random_var_tuple = (Tuple(Any[spawn_prey,prey2pred,pred_dies,theta])...,
+                Tuple(x for x in hazards_list)...,
+                Tuple(x for x in prey_list)...,
+                Tuple(x for x in pred_list)...)
+samples = rand(random_var_tuple, random_var_tuple[N+500] < 50.00,
                 100, alg = RejectionSample)
 
 compile = []
 for x in 1:100
-    push!(compile, samples[x][15000])
+    push!(compile, samples[x][N+500])
 end
-histogram(compile)
+histogram(compile, bins = 10)
+
+compile = []
+for x in 1:100
+    push!(compile, samples[x][N*2+999])
+end
+histogram(compile, bins = 10)
 
 # extract run results and plot
 prey_vals = []
@@ -174,3 +183,25 @@ plot(hcat(prey_vals,pred_vals),
         title = "Intervention Simulation",
         label = ["Prey" "Pred"],
         lw = 1.25)
+
+## Test
+x = normal(100, 10)
+function deterministic(rng)
+    if x(rng) > 100
+        x(rng) + 10
+    else
+        x(rng) - 50
+    end
+end
+deterministic_ = ciid(deterministic)
+function deterministic_two(rng)
+    deterministic_(rng) - 10
+end
+deterministic_two_ = ciid(deterministic_two)
+tuple_test = (x, deterministic_, deterministic_two_)
+samples = rand(tuple_test, tuple_test[3]<110, 100, alg = RejectionSample)
+compile = []
+for x in 1:100
+    push!(compile, samples[x][3])
+end
+histogram(compile, bins = 10)
